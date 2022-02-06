b08226e26d45e3f5457182151ea50b192f3d0e5c
Oscar Lahaie <oscarlahaie83@gmail.com>
2022-02-06 15:42:12 +0100

    test

--- test/test.ml
+++
@@ -1,9 +0,0 @@
-open SearchTree;;
-
-let tree_completed = Noeud(Noeud (Noeud (Vide,Int_elt 1,Vide), Int_elt 2, Noeud (Vide,Int_elt 3, Vide)), Int_elt 4, Noeud (Noeud (Vide, Int_elt 5,Vide), Int_elt 6, Noeud (Vide, Int_elt 7, Vide)));;
-
-let tree = Noeud (Vide, Int_elt 5, Vide);;
-
-let tree2 = add (Int_elt 4) tree;;
-
-let tree3 = add (Int_elt 6) tree2;;
