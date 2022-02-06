369824e12f590a71cff2611e9157e055067063de
Oscar Lahaie <oscarlahaie83@gmail.com>
2022-02-06 15:48:18 +0100

    test

--- test/test.ml
+++ test/test.ml
@@ -1,3 +1,5 @@
+open SearchTree;;
+
 let tree_completed = Noeud(Noeud (Noeud (Vide,Int_elt 1,Vide), Int_elt 2, Noeud (Vide,Int_elt 3, Vide)), Int_elt 4, Noeud (Noeud (Vide, Int_elt 5,Vide), Int_elt 6, Noeud (Vide, Int_elt 7, Vide)));;
 print_endline (string_of_bool(is_correct tree_completed));;
 
